2bd6f26bbf9d9295dbd3662ae6fed803ea401d3d
SaiLakshmiBharathiJandhyala <bharathijandhyala1974@gmail.com>
2019-03-14 21:02:21 +0530

    widgets app

--- app/src/main/java/com/example/widgets/MainActivity.java
+++ app/src/main/java/com/example/widgets/MainActivity.java
@@ -1,13 +1,49 @@
 package com.example.widgets;
 
+import android.appwidget.AppWidgetManager;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+    ListView listView;
+    SharedPreferences preferences;
+    SharedPreferences.Editor editor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        listView = findViewById(R.id.list);
+        preferences = getSharedPreferences(getPackageName(), MODE_PRIVATE);
+        editor = preferences.edit();
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String s = parent.getItemAtPosition(position).toString();
+                /*you can also get the same function with this code
+                 * String s=parent.getItemAtPosition(position).toString()*/
+                editor.putString(getPackageName(), s);
+                /* you can use apply() instead of commit on shared preferences.
+                 *as commit blocks and writes its data to persistent storage immediately,
+                 *apply will handle it in the background. */
+                editor.apply();
+
+
+                Intent intent = new Intent(MainActivity.this, SampleWidget.class);
+                intent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
+                int[] ids = AppWidgetManager.getInstance(MainActivity.this).getAppWidgetIds(new ComponentName(getApplicationContext(), SampleWidget.class));
+                intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, ids);
+                sendBroadcast(intent);
+
+
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/widgets/SampleWidget.java
+++ app/src/main/java/com/example/widgets/SampleWidget.java
@@ -5,18 +5,26 @@ import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.widget.RemoteViews;
 
+import static android.content.Context.MODE_PRIVATE;
+
 public class SampleWidget extends AppWidgetProvider {
+
+    SharedPreferences preferences;
+
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-//        super.onUpdate(context, appWidgetManager, appWidgetIds);
         for (int appWidget : appWidgetIds) {
+            preferences = context.getSharedPreferences(context.getPackageName(), MODE_PRIVATE);
             Intent i = new Intent(context, MainActivity.class);
             PendingIntent pi = PendingIntent.getActivity(context, 20, i, 0);
             RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.widegt_design);
-            remoteViews.setOnClickPendingIntent(R.id.widgets, pi);
+            remoteViews.setOnClickPendingIntent(R.id.list, pi);
+            remoteViews.setTextViewText(R.id.tv, preferences.getString(context.getPackageName(), "no item selected"));
             appWidgetManager.updateAppWidget(appWidget, remoteViews);
+
         }
     }
 }
